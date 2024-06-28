b06088129b395aaa95ac5d6fb60ebe023f4a7880
lucasvec <dosgamesxxx@gmail.com>
2024-06-28 13:04:14 -0300

    starting method of changing product

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,6 +15,39 @@ class ProductController {
         return response.json(this.serviceProduct.findById(id));
     }
 
+    updateProduct(request, response){
+        const {id} = request.params;
+        const {name, price, category} = request.body;
+
+        const findProduct = this.serviceProduct.findById(id);
+
+        if(findProduct != null){
+            if(name != null){
+                product = {
+                    name: name
+                }
+                return product
+            }else{
+                if(price != null){
+                    product = {
+                        price: price
+                    }
+                    return product
+                }
+                else{
+                    if(category != null){
+                        product = {
+                            category: category
+                        }
+                        return product
+                    }
+                }
+            }
+        }
+
+
+    }
+
     insertProduct(request, response) {
         const { name, price, category } = request.body;
 
@@ -22,7 +55,7 @@ class ProductController {
         const existsProduct = products.find(product => product.name.toLowerCase().trim()  === name.toLowerCase().trim());
 
         if (existsProduct){
-            return response.json({message: "Error. Product already exists."});
+            return response.json({  error: "Product already exists."});
         }else{
 
             const product = {
@@ -37,7 +70,7 @@ class ProductController {
             if (result){
                 return response.json(product);
             }else{
-                return response.json({message: "Error. Product not inserted."});
+                return response.json({ error: "Product not inserted."});
             }
         }
 
@@ -50,9 +83,11 @@ class ProductController {
         if (result != null ) {
             return response.json(result[0]);
         } else {
-            return response.json({ error: 'Product not found' });
+            return response.json({ error: 'Product not found'});
         }
     }
+
+
 }
 
 module.exports = ProductController;
